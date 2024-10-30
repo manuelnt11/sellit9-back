eafdfcda3a36ed753d883f6887b058bac2043208
Manuel Nuñez <manuel.nunez@optimizely.com>
2024-10-29 19:54:33 -0500

    Just for check key

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
 import { FirestoreModule } from './firestore/firestore.module';
 import { RnmClientService } from './rnm-client/rnm-client.service';
+import * as fs from 'fs';
 
 @Module({
   imports: [
@@ -14,6 +15,10 @@ import { RnmClientService } from './rnm-client/rnm-client.service';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
         console.log('SA_KEY', configService.get<string>('SA_KEY'));
+        console.log(
+          'key content:',
+          fs.readFileSync(configService.get<string>('SA_KEY'), 'utf8'),
+        );
         return {
           projectId: configService.get<string>('PROJECT_ID'),
           keyFilename: configService.get<string>('SA_KEY'),
